4b26021c059a390aac1cf7655e023636e55a32d8
jerryang76 <jerryang76@yahoo.com.tw>
2017-01-10 11:54:06 +0800

    Update httplibtest.py

--- httplibtest.py
+++ httplibtest.py
@@ -1,8 +1,20 @@
 import httplib, ssl, sys
 
+# Ignore certicate-validation-urllib2
+#SRC : http://stackoverflow.com/questions/19268548/python-ignore-certicate-validation-urllib2
+#Context creation
+#https://docs.python.org/2/library/ssl.html#ssl.wrap_socket
 ctx = ssl.create_default_context()
+# Disable check hostname
+#https://docs.python.org/2/library/ssl.html#ssl.SSLContext.check_hostname
 ctx.check_hostname = False
+# No certificates will be required from the other side of the socket connection.
+# If a certificate is received from the other end, no attempt to validate it is made.
+#https://docs.python.org/2/library/ssl.html#ssl.SSLContext.verify_mode
+#https://docs.python.org/2/library/ssl.html#ssl.CERT_NONE
 ctx.verify_mode = ssl.CERT_NONE
+
+
 sub = '/LoginForm.asp'
 headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 count = 0
